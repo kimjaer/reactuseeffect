2f290cd3be13b17fc4cc29d6ec73d8f82392e6df
4thdraw <4thdraw2@gmail.com>
2023-12-27 18:02:16 +0900

    최적화

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
+//딱 한번 호출되는 식 ( 함수로 만들고 실행하므로 / 랜더링시 다시 호출 할 필요없음 )
+// 1회 한번
+// 화살표함수 
 const weekcon = () => [
   {
     yoil: "월",
@@ -37,18 +40,26 @@ const weekcon = () => [
     yoil: "일",
     subject: "일요일좋아",
     content: "왜벌써내일월요일?"
-  },
+  }
 ]
+// 외부 함수에 접근하기 위해서 함수로 선언
+
 
 function App() {
+  //이 함수는 컴포넌트라서 이 내부가 랜더링시 초기화되면서 return식을 새롭게 index.js 컴포넌트에게 호출되는 것
+  //이 컴포넌트는 useState가 2개라서 랜더링빈도가 심함
 
-  const [week, weekchange] = useState(0); //데이터가져오는 순번
+  const [week, weekchange] = useState(0); // 초기번호
 
-  const [isPaused, setPaused] = useState(false); // useEffect 통제 -> week 통제
+  const [isPaused, setPaused] = useState(false);
+  // useEffect 통제 -> isPaused값이 false일때만 증가식실행
 
-  const weekcontent = weekcon(); //내부안에 return으로 한번만 실행해줌
+  const weekcontent = weekcon(); //내부안에 return있는 return 함수 -> 가벼워짐 -> 요일데이터는 변함이 없으므로 함수실행으로 값을 전달
 
   const weekUpdate = () => {
+    // 다음 순번으로 새롭게 랜더링해서 변화된 화면 노출/ 단 언제나 들어주는 것은 아님
+    // isPaused값이 false일때만 증가식실행 week값이 변화하지 못하게 한 랜더링변수
+    // isPaused가 true가 되면 week값을 변화하지 못하게 해서 랜더링을 막음
     if (!isPaused) {
       let count = (week + 1) % weekcontent.length;
       weekchange(count);
@@ -56,21 +67,24 @@ function App() {
   }
 
   const handleItemClick = (index) => {
+    //버튼클릭시 isPaused값을 변경 -> true일때는 week
+    setPaused(true); // true값이 들어와서 useEffect 건너뜀
+    weekchange(index); // 번호변경하고 랜더링시작 그래서 강제로 실행함
 
-    weekchange(index);
-    setPaused(true);
-
-    setTimeout(() => {
-      setPaused(false);
-      weekchange(index);
+    setTimeout(() => { // 3초뒤에 
+      setPaused(false); // 값이 변경되면서 랜더링할때 weekUpdate함수실행 그래서 
+      //weekchange(index); 할필요없음 useEffect에서 있음
     }, 3000)
 
   }
 
   useEffect(() => {
     const intervalId = setInterval(weekUpdate, 2000);
+    // isPaused에 따라 실행과 실행안함을 번갈아 가면서 진행
     return () => clearInterval(intervalId);
   }, [week, isPaused]);
+
+
 
 
 
