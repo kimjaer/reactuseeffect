f2a73b31f1991422973375046b34e28d662efc03
kimjaer <y5g5776@naver.com>
2023-12-28 10:58:58 +0900

    map으로 배너만들기

--- src/App.js
+++ src/App.js
@@ -2,116 +2,100 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-//딱 한번 호출되는 식 ( 함수로 만들고 실행하므로 / 랜더링시 다시 호출 할 필요없음 )
-// 1회 한번
-// 화살표함수 
+
 const weekcon = () => [
   {
-    yoil: "월",
+    yoil: "UXUI 아키텍쳐",
     subject: "월요일싫어",
-    content: "그냥월요일싫어",
-    bannersrc: "rgb(239 82 83)"
+    content: "클라이언트 요구사항를 반영한 피그마제공 | 협업을 위한 포지션별 업무분활",
+    bannersrc: "#13cfb8"
   },
   {
-    yoil: "화",
-    subject: "화요일싫어",
-    content: "그냥화요일싫어",
-    bannersrc: "#ff9671"
+    yoil: "WEB",
+    subject: "그누보드의 공통파일와 공통변수로 빠른 제작",
+    content: "비전문가를 위한 유지보수 | 메뉴얼제공 ",
+    bannersrc: "#ed145b"
   },
   {
-    yoil: "수",
-    subject: "수요일싫어",
+    yoil: "App",
+    subject: "리액트 프론트앤드 프로젝트",
     content: "그냥수요일싫어",
-    bannersrc: "#ffc75f"
+    bannersrc: "#ff783a"
   },
   {
-    yoil: "목",
-    subject: "목요일싫어",
+    yoil: "App",
+    subject: "노드서버기반의 restFulApi 구현",
     content: "그냥목요일싫어",
-    bannersrc: "#f9f871"
-  },
-  {
-    yoil: "금",
-    subject: "금요일좋아",
-    content: "내일주말이야",
-    bannersrc: "#008f7a"
-  },
-  {
-    yoil: "토",
-    subject: "토요일좋아",
-    content: "근데왜이리빨리가?",
-    bannersrc: "#2c73d2"
-  },
-  {
-    yoil: "일",
-    subject: "일요일좋아",
-    content: "왜벌써내일월요일?",
-    bannersrc: "#f8452ec2"
+    bannersrc: "#6e7a95"
   }
+
 ]
-// 외부 함수에 접근하기 위해서 함수로 선언
+
+
 
 
 function App() {
-  //이 함수는 컴포넌트라서 이 내부가 랜더링시 초기화되면서 return식을 새롭게 index.js 컴포넌트에게 호출되는 것
-  //이 컴포넌트는 useState가 2개라서 랜더링빈도가 심함
 
-  const [week, weekchange] = useState(0); // 초기번호
 
-  const [isPaused, setPaused] = useState(false);
-  // useEffect 통제 -> isPaused값이 false일때만 증가식실행
+  const [week, weekchange] = useState(0); // 나머지공식 
+  const [isPaused, setPaused] = useState(false); //토글기능
 
-  const weekcontent = weekcon(); //내부안에 return있는 return 함수 -> 가벼워짐 -> 요일데이터는 변함이 없으므로 함수실행으로 값을 전달
+  const weekcontent = weekcon();
 
   const weekUpdate = () => {
-    // 다음 순번으로 새롭게 랜더링해서 변화된 화면 노출/ 단 언제나 들어주는 것은 아님
-    // isPaused값이 false일때만 증가식실행 week값이 변화하지 못하게 한 랜더링변수
-    // isPaused가 true가 되면 week값을 변화하지 못하게 해서 랜더링을 막음
+    //
+
     if (!isPaused) {
       let count = (week + 1) % weekcontent.length;
-      weekchange(count);
+      //랜더링할때 마다 초기화되는 변수에는 반드시 useState변수가 들어있다
+      weekchange(count); //증가 순환발생됨
     }
   }
 
   const handleItemClick = (index) => {
-    //버튼클릭시 isPaused값을 변경 -> true일때는 week
-    setPaused(true); // true값이 들어와서 useEffect 건너뜀 week번호 그래도라서 요일 안바뀜
-    weekchange(index); // 번호변경하고 랜더링시작 그래서 강제로 실행함
-
-    setTimeout(() => { // 3초뒤에 자동실행 시작됨
-      setPaused(false); // 값이 변경되면서 랜더링할때 weekUpdate함수실행 그래서 
-      //weekchange(index); 할필요없음 useEffect에서 있음
+    setPaused(true);
+    weekchange(index);
+    setTimeout(() => {
+      setPaused(false);
     }, 3000)
 
   }
+  const widthset = document.body.offsetWidth * weekcontent.length
 
   useEffect(() => {
     const intervalId = setInterval(weekUpdate, 2000);
-    // isPaused에 따라 실행과 실행안함을 번갈아 가면서 진행
-    // 2개 변경변수가 바뀌면 진행되지만
-    // isPaused값이 true이면 week값이 변경되지않아서 자동롤링을 못한다.( 랜더링 안해주니깐 )
     return () => clearInterval(intervalId);
   }, [week, isPaused]);
 
 
-
-
-
-
   return (
-    <div className="App">
+    // weekcon.map((e, i) => {
+      
+    // })
+    <div className="App" style={{overflow:'hidden'}}>
+      <div style={{ display: "flex", width: widthset }}>
+        {
+          weekcontent.map((v, i) => {
+            return <div key={i} style={{ height: "50vh", width: document.body.offsetWidth ,backgroundColor: v.bannersrc }}>
+          <div>
+            <strong>{v.subject}</strong>
+            <p>{v.content}</p>
+          </div>
+            </div>
+          })
+        }
+      </div>
+
       <ul className='d-flex tablist'>
         {
           weekcontent.map((v, i) => {
-            return <li key={i} onClick={() => handleItemClick(i)} className={week === i ? "act" : null}>{v.yoil}요일</li>
+            return <li key={i} onClick={() => handleItemClick(i)}
+              className={week === i ? "act" : null}>{v.yoil}요일</li>
 
           })
         }
       </ul>
-      <div>
-        <strong>{weekcontent[week].subject}</strong>
-        <p>{weekcontent[week].content}</p>
-      </div>
+
     </div>
   );
 }
